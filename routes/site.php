13b7427f3670243a22b489c924e5b5734e908a54
Pedro Lopes <pedrolopes.info@gmail.com>
2018-05-13 01:23:58 -0300

    Site - Produtos

--- routes/site.php
+++ routes/site.php
@@ -2,10 +2,14 @@
 
 	use \Hcode\Page;
 	use \Hcode\Model\Category;
+	use \Hcode\Model\Product;
 
 	$app->get('/', function() {
+		$products = Product::listAll();
 		$page = new Page();
-		$page->setTpl("index");
+		$page->setTpl("index", array(
+			"products"=>Product::checkList($products)
+		));
 	});
 	
 	$app->get("/categories/:idcategory", function($idcategory) {
